03dfa33699c290599f5861cd1c4773ebb1ae4e1b
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-04-10 21:00:25 +0000

    Initialize test controller

--- backend/Backend/src/main/java/project/controllers/Test.java
+++ backend/Backend/src/main/java/project/controllers/Test.java
@@ -1,4 +1,14 @@
 package project.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test {
+
+    @GetMapping(value = {"/welcome"})
+    public String welcome(){
+        return "Hello Hassan and Youssef";
+    }
 }
