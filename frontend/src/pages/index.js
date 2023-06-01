5e2eadc2c5f888c0b2e91305c01492aed28a6c0c
Elab4d1 <hassanflv@gmail.com>
2023-06-01 15:55:47 +0100

    initialize frontend project

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,9 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-20 ${inter.className}`}
     >
-      Lherba{" "}
+      Lherba
     </main>
   );
 }
