0e7d3d78f749ab22b231850f05cc729b28698ac5
Elab4d1 <hassanflv@gmail.com>
2023-06-02 19:05:21 +0100

    fix auth

--- backend/src/main/java/project/payload/request/RegisterRequest.java
+++ backend/src/main/java/project/payload/request/RegisterRequest.java
@@ -21,7 +21,7 @@ public class RegisterRequest {
     private String name;
 
     @NotBlank(message = "Mobile number must not be blank")
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    @Pattern(regexp = "(^(?:\\+|00)212[ -]?\\d{2}[ -]?\\d{3}[ -]?\\d{2}[ -]?\\d{2}$)", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
     @NotBlank(message = "Email must not be blank")
